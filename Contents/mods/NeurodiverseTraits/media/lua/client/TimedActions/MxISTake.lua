ba3496c6982624e0590023e0ebb4d6df49e84aa8
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2022-04-15 13:32:47 +0100

    useNeuroTraitPills first implementation

---
+++ Contents/mods/NeurodiverseTraits/media/lua/client/TimedActions/MxISTake.lua
@@ -0,0 +1,18 @@
+require "TimedActions/ISBaseTimedAction"
+
+local old_ISTakePillAction_perform = ISTakePillAction.perform
+function ISTakePillAction:perform()
+	local result = old_ISTakePillAction_perform(self)
+	if self.item:getModule() == 'NeuroTraits' then
+		self:useNeuroTraitPills()
+		self.item:Use();
+	end
+	return result
+end
+
+function ISTakePillAction:useNeuroTraitPills()
+	local type = self.item:getType()
+	if type == "PillsRitalarr" then
+		print('PillsRitalarr :)')
+	end
+end
